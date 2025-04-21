0091074f9cac9f4e28dc8861867d357139c47d6d
Ayslan_Gamedev <ayslan_gamedev@disroot.org>
2025-04-21 16:13:16 -0300

    Add Patrol feature to enemys

--- scenes/actors/enemys/enemy.gd
+++ scenes/actors/enemys/enemy.gd
@@ -1,10 +1,39 @@
 class_name Enemy
 extends CharacterBody2D
 
-@onready var _state_machine := $StateMachine
+@export var points: Array[Node2D]
 
+@onready var _state_machine := $StateMachine
 @onready var _controller := $EnemyController
 @onready var _jump := $JumpComponent
+
+
+func _ready() -> void:
+	_controller.points = points
+
+	var states: Dictionary = {
+		"Idle": StateData.new($StateMachine/OnIdle, {
+				"Fall": _is_falling,
+				"Walk": func(): return _controller.direction.x != 0.0,
+		}),
+		"Walk": StateData.new($StateMachine/OnWalk, {
+			"Fall": _is_falling,
+			"Idle": func(): return _controller.direction.x == 0.0,
+		}),
+		"Fall": StateData.new($StateMachine/OnFall, {
+			"Idle": is_on_floor,
+		}),
+	}
+
+	_state_machine.setup("Idle", states)
+
+
+func _physics_process(_delta: float) -> void:
+	move_and_slide()
+
+	if is_on_floor():
+		_jump.reset()
+		_controller.patrol()
 
 
 func _is_falling() -> bool:
