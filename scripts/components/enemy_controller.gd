c83f5ade3b04ed9f20a6eaa1a74247a557ffaa95
Ayslan_Gamedev <ayslan_gamedev@disroot.org>
2025-04-21 16:13:16 -0300

    Add Patrol feature to enemys

--- scripts/components/enemy_controller.gd
+++ scripts/components/enemy_controller.gd
@@ -1,18 +1,29 @@
 class_name EnemyController
 extends Controller
 
+# Points the enemy will patrol between
 var points: Array[Node2D]
 
-
-var prev_direction: float
+var _curr_target = 0
+var _target: Node2D
 
 
 func _enter_tree() -> void:
 	owner.set_meta(&"Controller", self)
 
+
 func _ready() -> void:
 	if points.size() != 0:
 		print("point 1: ", points[0].position)
 
-	
-# here I altern the direction
+
+func patrol() -> void:
+	if points.size() != 0:
+		if _target == null:
+			_target = points[0]
+		
+		if abs(_target.position.x - owner.position.x) <= 2.0:
+			_curr_target = (_curr_target + 1) % points.size()
+			_target = points[_curr_target]
+					
+		direction.x = sign(_target.position.x - owner.position.x)
